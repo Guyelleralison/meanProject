b671298e0c3da16857b0606c278192590b71ff8d
Guyelleralison <ralisonguyelle@gmail.com>
2020-02-17 15:48:17 +0300

    get code source

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://guyellehelena:MeanProject@clustercinema-kndaz.mongodb.net/test?retryWrites=true');
+mongoose.set('debug', true);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+ 
+//Enable CORS
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+ next();
+});
+
+//Get environment port or use 3000
+const port = process.env.PORT || '3000';
+app.set('port', port);
+ 
+//Create HTTP server.
+const server = http.createServer(app);
+ 
+//Listen on port
+server.listen(port, () => console.log(`API running on localhost:${port}`));
